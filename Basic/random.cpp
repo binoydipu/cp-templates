50c3e52168be0457f0b3659e520b9bb4fd600bfb
Binoy Barman <binoydipu@gmail.com>
2024-11-15 00:16:53 +0600

    updated random

--- Basic/random.cpp
+++ Basic/random.cpp
@@ -3,6 +3,13 @@ int getRand(int l, int r) {
     uniform_int_distribution<int> uid(l, r);
     return uid(rng);
 }
+vector<int> generateRandomNumbers(int n, int l, int r) {
+    std::vector<int> randomNumbers;
+    for (int i = 0; i < n; ++i) {
+        randomNumbers.push_back(getRand(l, r));
+    }
+    return randomNumbers;
+}
 
 cout<<fixed<<setprecision(10);
 cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n"; 
