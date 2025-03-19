403b5c4a3d69d72c6b1dfdcdb6752290b201cb95
Binoy Barman <binoydipu@gmail.com>
2025-03-19 21:45:34 +0600

    minor add

--- Data Structure/dsu.cpp
+++ Data Structure/dsu.cpp
@@ -1,31 +1,33 @@
-const int N = 3e5 + 9;
-
-struct DSU {
+class DSU {
+  public:
     vector<int> par, rank, sz;
-    int c;
-    DSU(int n) : par(n + 1), rank(n + 1, 0), sz(n + 1, 1), c(n) {
-        for (int i = 1; i <= n; i++) par[i] = i;
+    int c, n;
+
+    DSU(int _n) : n(_n), par(_n + 1), rank(_n + 1), c(_n) {
+        iota(par.begin(), par.end(), 0);
+        sz.resize(n + 1, 1);
     }
-    int find(int v) { // finding root of v
-        if(par[v] == v) return v;
-        else return par[v] = find(par[v]);
+
+    inline int find(int v) { // finding root of v
+        return par[v] == v ? v : par[v] = find(par[v]);
     }
-    bool same(int a, int b) {
+    inline bool same(int a, int b) {
         return find(a) == find(b);
     }
-    int get_size(int v) {
+    inline int get_size(int v) {
         return sz[find(v)];
     }
-    int count() {
+    inline int count() {
         return c; // connected components
     }
-    void merge(int a, int b) {
+    inline bool unite(int a, int b) {
         a = find(a), b = find(b);
-        if(a == b) return; // already in same component
+        if(a == b) return false; // already in same component
         else c--;
         if(rank[a] > rank[b]) swap(a, b);
         par[a] = b;
         sz[b] += sz[a];
         if(rank[a] == rank[b]) rank[b]++;
+        return true;
     }
 };
