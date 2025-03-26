68edf35338bf3adcb8c068b97baac4032e0cdabe
Binoy Barman <binoydipu@gmail.com>
2025-03-26 14:19:34 +0600

    debug template updated

--- debug/debug.hpp
+++ debug/debug.hpp
@@ -1,85 +1,204 @@
+#ifdef LOCAL
+
 #include<bits/stdc++.h>
 using namespace std;
 
-template <typename A, typename B>string to_string(pair<A, B> p);
-template <typename A, typename B, typename C>string to_string(tuple<A, B, C> p);
-template <typename A, typename B, typename C, typename D>string to_string(tuple<A, B, C, D> p);
-string to_string(const string& s) { return '"' + s + '"'; }
-string to_string(char c) {string s;s += c;return s; }
-string to_string(const char* s) {return to_string((string) s); }
-string to_string(bool b) {return (b ? "1" : "0"); }
-string to_string(vector<bool> v) {bool first = true;string res = "{";for (int i = 0; i < static_cast<int>(v.size()); i++) {if (!first) {res += ", ";}first = false;res += to_string(v[i]);}res += "}";return res;}
-template <size_t N>string to_string(bitset<N> v) {string res = "";for (size_t i = 0; i < N; i++) {res += static_cast<char>('0' + v[i]);}return res;}
-template <typename A>string to_string(A v) {bool first = true;string res = "{";for (const auto &x : v) {if (!first) {res += ", ";}first = false;res += to_string(x);}res += "}";return res;}
-template <typename A, typename B>string to_string(pair<A, B> p) {return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";}
-template <typename A, typename B, typename C>string to_string(tuple<A, B, C> p) {return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";}
+// VS Code colors for the terminal
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define MAGENTA "\033[0;35m"
+#define CYAN "\033[0;36m"
+#define WHITE "\033[0;37m"
+#define RESET "\033[0m"
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
 template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p) {return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";}
-template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
-template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+string to_string(tuple<A, B, C, D> p);
 
+template <typename A>
+string to_string(queue<A> v);
 
-struct Point;
-string to_string(const Point& p);
+template <typename A>
+string to_string(stack<A> v);
 
-// ->
+template <typename A>
+string to_string(priority_queue<A> v);
+
+template <typename A>
+string to_string(priority_queue<A, vector<A>, greater<A>> v);
+
+string to_string(const string& s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string) s);
+}
+
+string to_string(char c) {
+    return "'" + string(1, c) + "'";
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto &x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+template <typename A>
+string to_string(queue<A> v) {
+    bool first = true;
+    string res = "[";
+    while(!v.empty()) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v.front());
+        v.pop();
+    }
+    res += "]";
+    return res;
+}
+
+template <typename A>
+string to_string(stack<A> v) {
+    bool first = true;
+    string res = "[";
+    while(!v.empty()) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v.top());
+        v.pop();
+    }
+    res += "]";
+    return res;
+}
+
+template <typename A>
+string to_string(priority_queue<A> v) {
+    bool first = true;
+    string res = "[";
+    while(!v.empty()) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v.top());
+        v.pop();
+    }
+    res += "]";
+    return res;
+}
+
+template <typename A>
+string to_string(priority_queue<A, vector<A>, greater<A>> v) {
+    bool first = true;
+    string res = "[";
+    while(!v.empty()) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v.top());
+        v.pop();
+    }
+    res += "]";
+    return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
 void debug_out() { cerr << endl; }
 
 template <typename Head, typename... Tail>
 void debug_out(Head H, Tail... T) {
-  cerr << " " << to_string(H);
-  debug_out(T...);
+    cerr << " " << to_string(H);
+    debug_out(T...);
 }
 
-// Print vector, deque, map, set
-#define dbg(...) cerr<<"Line:"<<__LINE__<<" "<< "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
-// <-
-
-
-// MINE->
-
-template<class T> using minheap = priority_queue<T, vector<T>, greater<T>>;
-
-// Print Time
-// #define time cout<<fixed<<setprecision(10); cerr<<"Time:"<<1000*((double)clock())/(double)CLOCKS_PER_SEC<<"ms\n"; 
-
-//Print array
-#define _print(v) {cerr<<"Line:"<<__LINE__<<" "<< "[" << #v << "]:"<<" [ "; for (auto &i:v){cerr<<i;cerr<<" ";}cerr<<"]";}
-
-template <typename K, typename V>
-void print(pair<K, V> p) { cerr << "(" << p.first << ", " << p.second << ")"; }
-
-template<typename T>
-void print(queue<T> v) { cerr<<"["; while(!v.empty()) { cerr<<v.front(); v.pop(); cerr<<(!v.empty() ? ", " : "]"); } }
-
-template<typename T>
-void print(stack<T> v) { cerr<<"["; while(!v.empty()) { cerr<<v.top(); v.pop(); cerr<<(!v.empty() ? ", " : "]"); } }
-
-template<typename T>
-void print(priority_queue<T> v) { cerr<<"["; while(!v.empty()) { cerr<<v.top(); v.pop(); cerr<<(!v.empty() ? ", " : "]"); } }
-
-template <typename K, typename V>
-void print(priority_queue<pair<K, V>> v) { cerr<<"["; while(!v.empty()) { print(v.top()); v.pop(); cerr<<(!v.empty() ? ", " : "]"); } }
-
-template<typename T>
-void print(minheap<T> v) { cerr<<"["; while(!v.empty()) { cerr<<v.top(); v.pop(); cerr<<(!v.empty() ? ", " : "]"); } }
-
-// print execution time
-string exec_time() { auto x = ceil(1000 * ((double)clock()) / (double)CLOCKS_PER_SEC); int z = x; return "Time:" + to_string(z) + "ms"; }
-
-#define _Time_ exec_time()
-void print(string s) { cerr << s; } 
-
-
-// ->
-void print_out() { cerr << endl; }
-
-template <typename Head, typename... Tail>
-void print_out(Head H, Tail... T) {
-  cerr << " "; print(H);
-  print_out(T...);
+string __Time() {
+    auto x = ceil(1000 * ((double)clock()) / (double)CLOCKS_PER_SEC);
+    int z = x;
+    return YELLOW + to_string(z) + "ms" + RESET;
 }
 
-// To print stack, queue, priority queue, execution Time
-#define print(...) cerr<<"Line:"<<__LINE__<<" "<< "[" << #__VA_ARGS__ << "]:", print_out(__VA_ARGS__)
-// <-
+#define _Time_ __Time()
+#define START_TIMER auto __start_time = chrono::high_resolution_clock::now();
+#define END_TIMER cerr << "Line:" << __LINE__ << YELLOW << " Execution Time: " << chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - __start_time).count() << "ms" << RESET << endl;
+
+#define dbg(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__);
+
+#else
+#define dbg(...) 42
+#define START_TIMER
+#define END_TIMER
+#define _TIME_
+#define RED
+#define RED
+#define GREEN
+#define YELLOW
+#define BLUE
+#define MAGENTA
+#define CYAN
+#define WHITE
+#define RESET
+#endif
--- template.cpp
+++ template.cpp
@@ -1,24 +1,24 @@
 /**
  *    author:   Binoy Barman
- *    created:  2025-03-04 18:26:57
+ *    created:  2025-03-26 14:18:07
 **/
 
 #include<bits/stdc++.h>
 #ifdef LOCAL
-#include "debug/debug.hpp"
+#include "algo/debug.h"
 #else
 #define dbg(...) 42
-#define print(...) 42
 #endif
 using namespace std;
 using ll = long long;
 const int mod = 1e9 + 7;
-const int inf = 1e9;
+const int inf = 2e9;
 
 #define int long long
 #define nl '\n'
 #define all(v) v.begin(), v.end()
-#define Testcase_Handler int tts, tc = 1; cin >> tts; hell: while(tts--)
+#define clg(x) (32 - __builtin_clz(x))
+#define Testcase_Handler int tts, tc = 0; cin >> tts; hell: while(tc++ < tts)
 #define uniq(v) sort(all(v)), v.resize(distance(v.begin(), unique(v.begin(), v.end())))
 #define loop(index, _start, _end) for (int index = (_start), _step = (_start) <= (_end) ? 1 : -1; index != (_end) + _step; index += _step)
 template<class T> using minheap = priority_queue<T, vector<T>, greater<T>>;
@@ -40,9 +40,9 @@ inline void init() {
 int32_t main() {
     Dark_Lord_Binoy::init();
     Testcase_Handler {
-        
+        ...
     }
 
-    print(_Time_);
+    dbg(_Time_);
     return 0;
 }
